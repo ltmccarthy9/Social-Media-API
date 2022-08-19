08b213956ac031bcd223c5803e64ae9686f3529a
Liam McCarthy <ltmccarthy9@gmail.com>
2022-08-18 20:27:41 -0500

    controllers

---
+++ controllers/userController.js
@@ -0,0 +1,22 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    //get all users
+    getUsers(req, res) {
+        User.find()
+        .then((users) => res.json(users))
+        .catch((err) => res.status(500).json(err));
+    },
+    //get a user
+    getSingleUser(req, res) {
+        User.findOne({_id: req.params.userID})
+        .select('-__v')
+        .then((user) =>
+            !user
+            ? res.status(404).json([ message: 'No user with that ID'])
+            : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    
+}
