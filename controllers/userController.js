c3353283b93f1ba550854ff80c039d96852950e7
Liam McCarthy <ltmccarthy9@gmail.com>
2022-08-19 00:18:28 -0500

    readme

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,8 @@ const { User } = require('../models');
 module.exports = {
     //get all users
     getUsers(req, res) {
-        User.find().then((users) => res.json(users))
+        User.find({})
+        .then((users) => res.json(users))
         .catch((err) => res.status(500).json(err));
     },
     //get a user
