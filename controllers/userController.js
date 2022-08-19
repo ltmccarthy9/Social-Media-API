f559ea789a8a3f27a039c0522e0092a192c6280b
Liam McCarthy <ltmccarthy9@gmail.com>
2022-08-18 20:48:22 -0500

    controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,10 +13,42 @@ module.exports = {
         .select('-__v')
         .then((user) =>
             !user
-            ? res.status(404).json([ message: 'No user with that ID'])
+            ? res.status(404).json({ message: 'No user with that ID'})
             : res.json(user)
             )
             .catch((err) => res.status(500).json(err));
     },
-    
-}
+    // post a new user
+    createUser(req, res) {
+        User.create(req.body)
+        .then((user) => res.json(user))
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+        });
+    },
+    // update a user
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userID },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+        .then((user) => 
+        !user
+        ? res.status(404).json({ message: 'No user with that ID' })
+        : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+    // delete a user
+    deleteUser(req, res) {
+        User.findOneAndDelete({_id: req.params.userID})
+        .then((user) => 
+        !user
+            ? res.status(404).json({ message: 'No user with that ID'})
+            : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+};
